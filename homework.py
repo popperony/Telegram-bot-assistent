64e4a224ff66ff79e9d11d8c5b9715c28ad6c78c
Вадим <popperony@yandex.ru>
2020-04-28 23:12:13 +0300

    v0.0.1

--- homework.py
+++ homework.py
@@ -6,15 +6,15 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-
-PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
+PROXY_URL = os.getenv('PROXY_URL')
+PRACTICUM_TOKEN = os.getenv("PRACTICUM_TOKEN")
 
 
 def parse_homework_status(homework):
-    homework_name = ...
-    if ...
+    homework_name = homework['homework_name']
+    if homework['status'] == "rejected":
         verdict = 'К сожалению в работе нашлись ошибки.'
     else:
         verdict = 'Ревьюеру всё понравилось, можно приступать к следующему уроку.'
@@ -22,26 +22,32 @@ def parse_homework_status(homework):
 
 
 def get_homework_statuses(current_timestamp):
-    ...
-    homework_statuses = ...
-    return homework_statuses.json()
+    params = {'from_date': current_timestamp}
+    headers = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+    url = 'https://praktikum.yandex.ru/api/user_api/homework_statuses/'
+    homework_statuses = requests.get(
+        url,
+        params=params,
+        headers=headers
+        ).json()
+    return homework_statuses
 
 
 def send_message(message):
-    ...
-    return bot.send_message(...)
+    proxy = telegram.utils.request.Request(proxy_url=PROXY_URL)
+    bot = telegram.Bot(token=TELEGRAM_TOKEN, request=proxy)
+    return bot.send_message(chat_id=CHAT_ID, text=message)
 
 
 def main():
-    current_timestamp = int(time.time())  # начальное значение timestamp
-
+    current_timestamp = int(time.time())
     while True:
         try:
             new_homework = get_homework_statuses(current_timestamp)
             if new_homework.get('homeworks'):
                 send_message(parse_homework_status(new_homework.get('homeworks')[0]))
-            current_timestamp = new_homework.get('current_date')  # обновить timestamp
-            time.sleep(300)  # опрашивать раз в пять минут
+            current_timestamp = new_homework.get('current_date')
+            time.sleep(300)
 
         except Exception as e:
             print(f'Бот упал с ошибкой: {e}')
